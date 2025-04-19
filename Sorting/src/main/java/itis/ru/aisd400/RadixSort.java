d49287ab61ac283272e40611329b857ef534a90b
Arina <mairabeeva42@gmail.com>
2025-04-19 22:33:25 +0300

    radix sort

---
+++ Sorting/src/main/java/itis/ru/aisd400/RadixSort.java
@@ -0,0 +1,98 @@
+package itis.ru.aisd400;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class RadixSort {
+    private static int iterationCount = 0;
+    public static int getMax(int arr[], int n) {
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            iterationCount++;
+        }
+        return max;
+    }
+
+    public static void countingSort(int[] arr, int position) {
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+
+        for (int i = 0; i < n; i++){
+            count[(arr[i]/position) % 10]++;
+            iterationCount++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] = count[i] + count[i - 1];
+            iterationCount++;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / position) % 10] - 1] = arr[i];
+            count[(arr[i]/position) % 10]--;
+            iterationCount++;
+        }
+
+        for (int i = 0; i < output.length; i++) {
+            arr[i] = output[i];
+            iterationCount++;
+        }
+    }
+
+    public static void print(int arr[], int n) {
+        for (int i = 0; i < n-1; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+    static void radixsort(int arr[], int n) {
+        iterationCount = 0;
+        long startTime = System.nanoTime();
+
+        int max = getMax(arr, n);
+        int position = 1;
+        while (max/position > 0) {
+            countingSort(arr, position);
+            position *= 10;
+            iterationCount++;
+        }
+
+        double time = (System.nanoTime() - startTime) / 1000.0;
+        System.out.println("Время выполнения в мкс: " + time);
+        System.out.println("Количество итераций: " + iterationCount);
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        for (int i = 1; i <= 60; i++) {
+            Scanner scanner = new Scanner(new File("random_datasets/dataset_" + i + ".txt"));
+            int[] tempNumbers = new int[10000];
+            int size = 0;
+
+            while (scanner.hasNextInt()) {
+                tempNumbers[size++] = scanner.nextInt();
+            }
+
+            scanner.close();
+
+            int[] numbers = new int[size];
+
+            for (int j = 0; j < size; j++) {
+                numbers[j] = tempNumbers[j];
+            }
+
+            System.out.println("Размер массива: " + size + " элементов");
+
+            radixsort(numbers, numbers.length);
+            print(numbers, numbers.length);
+        }
+    }
+}
+
